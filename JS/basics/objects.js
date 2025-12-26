89f507954ec265b7740c774e59c427a37639959a
sreeramgkrishnan <sreeramgkrishnan@gmail.com>
2025-12-26 16:44:21 -0500

    object in JS

---
+++ JS/basics/objects.js
@@ -0,0 +1,18 @@
+/**
+ * Understanding objects
+ */
+
+//declaration
+
+const user = {
+    name : 'SGK',
+    age : 30,
+    email : 'sgk@gmail.com',
+    isActive : true
+}
+
+//accession and printing specific values from the user object
+
+console.log(`name is ${user.name}`);
+console.log(`age is ${user.age}`);
+
